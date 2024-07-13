500068e29dbc949413cb08609cfb0dcbb9cc6fde
Maksym Sobolyev <sobomax@sippysoft.com>
2024-07-12 19:31:38 -0700

    Add RTPP_NODEBUG.

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 OUTBOUND_ROUTE="${OUTBOUND_ROUTE:-"0100@192.168.23.52;auth=nopass"}"
-RTPP_LOG_LEVEL="${RTPP_LOG_LEVEL:-"dbug"}"
+RTPP_LOG_LEVEL="${RTPP_LOG_LEVEL:-"info"}"
+RTPP_NODEBUG="${RTPP_NODEBUG:0}"
 
 CFILE="/webrtc_phone/server.crt"
 KFILE="/webrtc_phone/server.key"
@@ -21,9 +22,14 @@ BDIR="/webrtc_phone/b2bua"
 npm exec -- http-server -S -C "${CFILE}" -K "${KFILE}" -p ${PNUM} "${WROOT}" &
 HSERV_PID="${!}"
 
-/usr/local/bin/rtpproxy_debug -f -F -s "${RSOCK}" \
-  --dso "${RMODDIR}/rtpp_ice_lite_debug.so" \
-  --dso "${RMODDIR}/rtpp_dtls_gw_debug.so" \
+if [ ${RTPP_NODEBUG} -eq 0 ]
+then
+  RTPP_SUXX="_debug"
+fi
+
+/usr/local/bin/rtpproxy${RTPP_SUXX} -f -F -s "${RSOCK}" \
+  --dso "${RMODDIR}/rtpp_ice_lite${RTPP_SUXX}.so" \
+  --dso "${RMODDIR}/rtpp_dtls_gw${RTPP_SUXX}.so" \
   -d "${RTPP_LOG_LEVEL}" -m "${MIN_RTP_PORT}" -M "${MAX_RTP_PORT}" &
 RTPP_PID="${!}"
 
